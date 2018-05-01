b452d6ecbb3272829bfa61064d74db4ed6cb47e7
adgh82 <adgh130582@gmail.com>
2018-05-01 14:19:08 +0530

    Core Project

---
+++ ration-order/ration-order-core/src/main/java/com/homeutil/ration_order_core/App.java
@@ -0,0 +1,13 @@
+package com.homeutil.ration_order_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ration-order/ration-order-core/src/test/java/com/homeutil/ration_order_core/AppTest.java
@@ -0,0 +1,20 @@
+package com.homeutil.ration_order_core;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
